85c97a5ca99e438660e2bf410c5d62353129bf25
zentaras <zentarasofficial@gmail.com>
2026-05-27 00:06:12 +0530

    loader

---
+++ src/app/internship/InternshipLoader.js
@@ -0,0 +1,72 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { prisma } from "../../lib/prisma";
+import { redirect } from "next/navigation";
+import InternshipClient from "./InternshipClient";
+
+export default async function InternshipLoader({ searchParams }) {
+  const clerkUser = await currentUser();
+  if (!clerkUser) redirect("/sign-in");
+
+  const params      = await searchParams;
+  const applicantId = params?.applicantId;
+  if (!applicantId) redirect("/dashboard");
+
+  const [dataAnalyst, webDev] = await Promise.all([
+    prisma.dataAnalystApplicant.findFirst({
+      where: { id: applicantId, clerkUserId: clerkUser.id },
+    }),
+    prisma.webDevApplicant.findFirst({
+      where: { id: applicantId, clerkUserId: clerkUser.id },
+    }),
+  ]);
+
+  const applicant = dataAnalyst ?? webDev;
+  if (!applicant)                         redirect("/dashboard");
+  if (applicant.status !== "Shortlisted") redirect("/dashboard");
+
+  let projectKey, projectName;
+  if (dataAnalyst) {
+    projectKey  = "data-analyst-intern";
+    projectName = "Data Analyst Intern";
+  } else {
+    projectKey  = "web-dev-intern";
+    projectName = "Web Developer Intern";
+  }
+
+  let track = await prisma.internshipTrack.findUnique({
+    where: {
+      clerkUserId_applicantId: {
+        clerkUserId: clerkUser.id,
+        applicantId,
+      },
+    },
+    include: { steps: { orderBy: { stepNumber: "asc" } } },
+  });
+
+  if (!track) {
+    track = await prisma.internshipTrack.create({
+      data: {
+        clerkUserId: clerkUser.id,
+        applicantId,
+        projectKey,
+        projectName,
+        currentStep: 1,
+      },
+      include: { steps: { orderBy: { stepNumber: "asc" } } },
+    });
+  }
+
+  return (
+    <InternshipClient
+      clerkUser={{
+        id:        clerkUser.id,
+        firstName: clerkUser.firstName,
+        imageUrl:  clerkUser.imageUrl,
+      }}
+      applicant={JSON.parse(JSON.stringify(applicant))}
+      track={JSON.parse(JSON.stringify(track))}
+      projectKey={projectKey}
+      projectName={projectName}
+    />
+  );
+}
--- src/app/internship/page.js
+++ src/app/internship/page.js
@@ -1,74 +1,87 @@
-// app/internship/page.js
-import { currentUser } from "@clerk/nextjs/server";
-import { prisma } from "../../lib/prisma";
-import { redirect } from "next/navigation";
-import InternshipClient from "./InternshipClient";
+// // app/internship/page.js
+// import { currentUser } from "@clerk/nextjs/server";
+// import { prisma } from "../../lib/prisma";
+// import { redirect } from "next/navigation";
+// import InternshipClient from "./InternshipClient";
 
-export default async function InternshipPage({ searchParams }) {
-  const clerkUser = await currentUser();
-  if (!clerkUser) redirect("/sign-in");
+// export default async function InternshipPage({ searchParams }) {
+//   const clerkUser = await currentUser();
+//   if (!clerkUser) redirect("/sign-in");
 
-  const params      = await searchParams;
-  const applicantId = params?.applicantId;
-  if (!applicantId) redirect("/dashboard");
+//   const params      = await searchParams;
+//   const applicantId = params?.applicantId;
+//   if (!applicantId) redirect("/dashboard");
 
-  // Try each new table
-  const [dataAnalyst, webDev] = await Promise.all([
-    prisma.dataAnalystApplicant.findFirst({
-      where: { id: applicantId, clerkUserId: clerkUser.id },
-    }),
-    prisma.webDevApplicant.findFirst({
-      where: { id: applicantId, clerkUserId: clerkUser.id },
-    }),
-  ]);
+//   // Try each new table
+//   const [dataAnalyst, webDev] = await Promise.all([
+//     prisma.dataAnalystApplicant.findFirst({
+//       where: { id: applicantId, clerkUserId: clerkUser.id },
+//     }),
+//     prisma.webDevApplicant.findFirst({
+//       where: { id: applicantId, clerkUserId: clerkUser.id },
+//     }),
+//   ]);
 
-  const applicant = dataAnalyst ?? webDev;
-  if (!applicant)                         redirect("/dashboard");
-  if (applicant.status !== "Shortlisted") redirect("/dashboard");
+//   const applicant = dataAnalyst ?? webDev;
+//   if (!applicant)                         redirect("/dashboard");
+//   if (applicant.status !== "Shortlisted") redirect("/dashboard");
 
-  let projectKey, projectName;
-  if (dataAnalyst) {
-    projectKey  = "data-analyst-intern";
-    projectName = "Data Analyst Intern";
-  } else {
-    projectKey  = "web-dev-intern";
-    projectName = "Web Developer Intern";
-  }
+//   let projectKey, projectName;
+//   if (dataAnalyst) {
+//     projectKey  = "data-analyst-intern";
+//     projectName = "Data Analyst Intern";
+//   } else {
+//     projectKey  = "web-dev-intern";
+//     projectName = "Web Developer Intern";
+//   }
 
-  let track = await prisma.internshipTrack.findUnique({
-    where: {
-      clerkUserId_applicantId: {
-        clerkUserId: clerkUser.id,
-        applicantId,
-      },
-    },
-    include: { steps: { orderBy: { stepNumber: "asc" } } },
-  });
+//   let track = await prisma.internshipTrack.findUnique({
+//     where: {
+//       clerkUserId_applicantId: {
+//         clerkUserId: clerkUser.id,
+//         applicantId,
+//       },
+//     },
+//     include: { steps: { orderBy: { stepNumber: "asc" } } },
+//   });
 
-  if (!track) {
-    track = await prisma.internshipTrack.create({
-      data: {
-        clerkUserId: clerkUser.id,
-        applicantId,
-        projectKey,
-        projectName,
-        currentStep: 1,
-      },
-      include: { steps: { orderBy: { stepNumber: "asc" } } },
-    });
-  }
+//   if (!track) {
+//     track = await prisma.internshipTrack.create({
+//       data: {
+//         clerkUserId: clerkUser.id,
+//         applicantId,
+//         projectKey,
+//         projectName,
+//         currentStep: 1,
+//       },
+//       include: { steps: { orderBy: { stepNumber: "asc" } } },
+//     });
+//   }
 
+//   return (
+//     <InternshipClient
+//       clerkUser={{
+//         id:        clerkUser.id,
+//         firstName: clerkUser.firstName,
+//         imageUrl:  clerkUser.imageUrl,
+//       }}
+//       applicant={JSON.parse(JSON.stringify(applicant))}
+//       track={JSON.parse(JSON.stringify(track))}
+//       projectKey={projectKey}
+//       projectName={projectName}
+//     />
+//   );
+// }
+
+
+import { Suspense } from "react";
+import InternshipLoader from "./InternshipLoader";
+import Spinner from "../Components/Spinner";
+
+export default function InternshipPage({ searchParams }) {
   return (
-    <InternshipClient
-      clerkUser={{
-        id:        clerkUser.id,
-        firstName: clerkUser.firstName,
-        imageUrl:  clerkUser.imageUrl,
-      }}
-      applicant={JSON.parse(JSON.stringify(applicant))}
-      track={JSON.parse(JSON.stringify(track))}
-      projectKey={projectKey}
-      projectName={projectName}
-    />
+    <Suspense fallback={<Spinner />}>
+      <InternshipLoader searchParams={searchParams} />
+    </Suspense>
   );
 }
